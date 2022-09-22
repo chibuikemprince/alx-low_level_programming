9eb861c995714ddf6492bf3ad4d7af7b19c103cd
root <root@DESKTOP-QI6H2EA.localdomain>
2022-09-22 23:25:06 +0100

    Task done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-/**                                                                                                                      * main - Entry point                                                                                                    * Return: Always 0 (Success)                                                                                            */                                                                                                                     int main(void)                                                                                                          { 
+/**
+ * main - Entry point                                                                                                    * Return: Always 0 (Success)                                                                                            */                                                                                                                     int main(void)                                                                                                          { 
 	int d;
 
 	for (d = '0'; d <= '9'; d++)
