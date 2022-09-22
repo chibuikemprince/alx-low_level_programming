d10c2918d51228b3f29097f6ae805df34b440c10
root <root@DESKTOP-QI6H2EA.localdomain>
2022-09-22 15:15:53 +0100

    Task done

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-* main - Main function
+*main - Main function
 */
 int main(void)
 {
 char name[] =  "\"Programming is like building a multilingual puzzle";
-	
+
 puts(name);
 return (0);
 }
