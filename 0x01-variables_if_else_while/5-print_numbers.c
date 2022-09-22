003e8d516979268457dd13de4c18507fcf6f031f
root <root@DESKTOP-QI6H2EA.localdomain>
2022-09-22 22:38:13 +0100

    Task done

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int low;
+
+	for (low = 0; low < 10; low++)
+		printf("%d",low);
+	putchar('\n');
+	return (0);
+}
